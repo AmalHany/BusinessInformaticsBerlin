7e0a3f969496a71f7e431879fbd2382a3fe8b640
omarfayez95 <omarfayez.20@gmail.com>
2016-04-29 16:51:51 +0200

    Added Documentation, References, and Steps

--- sprint1team1/server/models/signup.js
+++ sprint1team1/server/models/signup.js
@@ -1,4 +1,4 @@
-var mongoose = require ('mongoose');
+var mongoose = require ('mongoose'); //This our mongoose call like in the server.js
 
 module.exports = mongoose.model ('User', {
 	email : String,
@@ -7,3 +7,9 @@ module.exports = mongoose.model ('User', {
 	lastName : String,
 	age : String
 });
+
+// module.exports makes the method callable via the require methods, you can see examples in the server
+/* The mongoose model function gives it a name and the structure of our collection, this can get way more detailed, like 
+setting the uniqueness of a parameter or if its required or not etc 
+This is the same process in the bdraft.js
+*/
