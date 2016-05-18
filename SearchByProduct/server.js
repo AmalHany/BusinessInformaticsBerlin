383ac31fd6d9acc24a1f24acc6dd3b952f36cf83
Your Name <your_email@whatever.com>
2016-05-18 15:11:28 +0200

    #85 Searching a product by category

--- SearchByProduct/server.js
+++
@@ -1,6 +0,0 @@
-var app     = require('./app/app');
-
-app.listen('3000', function(){
-  console.log('[OK] => HTTP Server listening on http://localhost:3000');
-  require('./app/db').init('mongodb://localhost:27017/Sprint2Team2' );
-});
