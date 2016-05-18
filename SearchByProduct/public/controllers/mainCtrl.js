a109d97a11967195fb70eb391a43f6a5ebc766ba
Your Name <your_email@whatever.com>
2016-05-18 14:28:15 +0200

    #85 Creating Html and search bar function

---
+++ SearchByProduct/public/controllers/mainCtrl.js
@@ -0,0 +1,35 @@
+/**
+ * Main Controller
+ */
+
+App.controller('mainCtrl', function($scope,[ProductsSrv,BrandsSrv], $location) {
+
+
+  /*----------- Angular Bootstrap Typeahead -----------*/
+
+  /* Retrieve List of Product names */
+  // Type ahead 
+  function ProductCodes() {
+    ProductsSrv.getProductCodes().success(function(product) {
+         $scope.Products = product;
+         // scope is the list of products
+     });
+  };
+ 
+
+  /* Record User's Selected Product  */
+  $scope.SetProduct = function(product) {
+    ProductsSrv.setSelectedProduct(product);
+  };
+
+ 
+  /* Find All Available Product  */
+  $scope.SearchProducts = function() {
+    $location.url('/products');
+    // Redirect to the next page 
+  };
+
+  /* Get Products on page render  */
+  ProductCodes();
+ 
+});
