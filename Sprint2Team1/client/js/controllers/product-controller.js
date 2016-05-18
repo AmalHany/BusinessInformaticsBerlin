330f13c784bbdd2110cf618ee474bf606a958ba9
NouranAdel <nouranadel94@gmail.com>
2016-05-18 20:38:26 +0200

    #81 fully implemented the category option, along with the database connection. Story Completed

--- Sprint2Team1/client/js/controllers/product-controller.js
+++ Sprint2Team1/client/js/controllers/product-controller.js
@@ -16,11 +16,13 @@ app.controller('prodController', ['$scope', '$resource', function ($scope, $reso
 		product.prodName = $scope.prodName;	
 		product.prodPrice = $scope.prodPrice;	
 		product.prodDesc = $scope.prodDesc;
+		product.prodCateg = $scope.prodCateg;	//Added by Nouran
 		product.$save(function (result){		//this saves the new user, and passes a function that takes the results as a parameter
 			$scope.product.push(result);		//Pushes the result into the collection user
 			$scope.prodName = '';			//This just resets our html  variables to nothing
 			$scope.prodPrice = '';			//This exact process is repeated in the businessdraft controller
 			$scope.prodDesc = '';
+			$scope.prodCateg = '';		//Added by Nouran
 		});
 	}
 }]);
--- Sprint2Team1/server/models/product.js
+++ Sprint2Team1/server/models/product.js
@@ -3,5 +3,6 @@ var mongoose = require ('mongoose');
 module.exports = mongoose.model ('product', {
 	prodName : String,
 	prodPrice: String,
-	prodDesc: String
+	prodDesc: String,
+	prodCateg: String //Added by Nouran
 });
