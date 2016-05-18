98743396be9bec0ebd3fcaab7e5f283bee1e520d
Your Name <mohanad_zahran@hotmail.com>
2016-05-18 18:37:09 +0200

    #82 Implemented HTML page and angular module

---
+++ Sprint2Team1/client/js/controllers/product-controller.js
@@ -0,0 +1,26 @@
+app.controller('prodController', ['$scope', '$resource', function ($scope, $resource) {
+	var Product = $resource('/api/products');
+
+
+	Product.query(function (results) {
+    $scope.products = results;
+  });
+
+/*
+	This query just calls ever user in our collection users, and stores them into an array reults
+*/
+	$scope.products = [] //This function just empties the array
+
+	$scope.createProduct = function () {		
+		var product = new Product();			
+		product.prodName = $scope.prodName;	
+		product.prodPrice = $scope.prodPrice;	
+		product.prodDesc = $scope.prodDesc;
+		product.$save(function (result){		//this saves the new user, and passes a function that takes the results as a parameter
+			$scope.product.push(result);		//Pushes the result into the collection user
+			$scope.prodName = '';			//This just resets our html  variables to nothing
+			$scope.prodPrice = '';			//This exact process is repeated in the businessdraft controller
+			$scope.prodDesc = '';
+		});
+	}
+}]);
