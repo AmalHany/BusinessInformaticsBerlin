acb06f014c2b2ac74d92caae821b1daf913d7c97
Your Name <your_email@whatever.com>
2016-05-21 16:00:17 +0200

    #85 updating html and integrating with team members

--- MahmoudSearchByProduct/public/services/productsSrv.js
+++ MahmoudSearchByProduct/public/services/productsSrv.js
@@ -3,7 +3,9 @@
  // linking between the controller and the server side 
  */
 App.factory('ProductsSrv', function ($http) {
+
      return {
+
          getBrands : function() {
            return $http.get('/api/data/brands');
          },
@@ -11,13 +13,20 @@ App.factory('ProductsSrv', function ($http) {
            return $http.get('/api/data/products');
          }, 
           getProduct : function(selectedProduct) {
-           return $http.get('/api/search/' + selectedProduct);
+            console.log(this.selectedCategory);
+           return $http.get('/api/search/' + this.selectedProduct +"?catg=" + this.selectedCategory);
          },
          setSelectedProduct: function(value) {
            this.selectedProduct = value;
          },
          getSelectedProduct: function() {
            return this.selectedProduct;
+         },
+         setSelectedCategory: function(value) {
+           this.selectedCategory = value;
+         },
+         getSelectedCategory: function() {
+           return this.selectedCategory;
          }
      };
  });
